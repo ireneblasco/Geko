d5bc31e4b0a7127340f55a3e0b8a4ba07d97c696
Irene <ireneblascom@gmail.com>
2025-09-19 20:00:49 -0700

    Initial Commit

---
+++ Geko/GekoApp.swift
@@ -0,0 +1,17 @@
+//
+//  GekoApp.swift
+//  Geko
+//
+//  Created by Irenews on 9/19/25.
+//
+
+import SwiftUI
+
+@main
+struct GekoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ GekoTests/GekoTests.swift
@@ -0,0 +1,17 @@
+//
+//  GekoTests.swift
+//  GekoTests
+//
+//  Created by Irenews on 9/19/25.
+//
+
+import Testing
+@testable import Geko
+
+struct GekoTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
